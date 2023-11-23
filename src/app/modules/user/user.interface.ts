c20a3c70fe134e77ad26a881daba1458fb44fa7d
Mohammad Rafe <mrrafi5869@gmail.com>
2023-11-23 18:01:56 +0600

    first commit

---
+++ src/app/modules/user/user.interface.ts
@@ -0,0 +1,25 @@
+export interface IFullName {
+    firstName: string,
+    lastName: string
+}
+
+export interface IAddress {
+    street: string,
+    city: string,
+    country: string
+}
+
+export interface IUser {
+    userId: number,
+    username: string,
+    password: string,
+    fullName: IFullName,
+    age: number,
+    email: string,
+    isActive: boolean,
+    hobbies: [
+        string,
+        string
+    ],
+    address: IAddress
+}
